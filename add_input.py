5685dd80c4fe4de2a7f80641b1fe2e8d34d1c7fb
IrinaKonopl <irina.konolyanova@gmail.com>
2024-12-07 20:23:19 +0300

    Add files via upload

---
+++ add_input.py
@@ -0,0 +1,7 @@
+username=input('Введите имя пользователя  ')
+title=input('Введите заголовок заметки  ')
+content=input('Введите описание заметки  ')
+status=input('Введите статус заметки  ')
+created_date=input('Введите дату создания заметки в формате dd:mm:yy ')
+issue_date=input('Введите дату окончания в формате dd:mm:yy  ')
+print (username, title, content, status, created_date, issue_date)
---
+++ data_changer.py
@@ -0,0 +1,9 @@
+# Преобразование даты в дату без года
+def d_w_y(d):
+    if len(d)==8:
+        return d[:-3]
+    else:
+        return d[:-5]
+
+dat=input('Введите дату ')
+print('Дата без года',d_w_y(dat))
